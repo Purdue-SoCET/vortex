40af7af6356a58bfcad7a792f252b8254208d4e8
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-02-15 11:08:53 -0500

    Update VX_to_mem_vortex_init.sv

--- hw/tb/VX_to_mem_vortex_init.sv
+++ hw/tb/VX_to_mem_vortex_init.sv
@@ -1,7 +1,9 @@
 module VX_to_mem_vortex_init();
 
+  Vortex vortex();
   
-
+  VX_to_mem_bypass RAM(clk, reset, mem_req_valid, mem_req_ready, mem_req_rw, mem_req_addr, mem_req_tag, mem_req_data, mem_rsp_valid, mem_rsp_data, mem_rsp_tag, mem_rsp_ready);
+);
 
 
 endmodule
