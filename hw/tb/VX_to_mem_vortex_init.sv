1699af33b47ae1e2bd3fc4afb5d0ff6a6d727890
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-02-15 11:10:27 -0500

    Update VX_to_mem_vortex_init.sv

--- hw/tb/VX_to_mem_vortex_init.sv
+++ hw/tb/VX_to_mem_vortex_init.sv
@@ -1,8 +1,35 @@
 module VX_to_mem_vortex_init();
 
-  Vortex vortex();
+  Vortex vortex(.clk            (clk),
+                .reset          (reset),
+
+                .mem_req_valid  (mem_req_valid),
+                .mem_req_rw     (mem_req_rw),
+                .mem_req_byteen (mem_req_byteen),
+                .mem_req_addr   (mem_req_addr),
+                .mem_req_data   (mem_req_data),
+                .mem_req_tag    (mem_req_tag),
+                .mem_req_ready  (mem_req_ready),
+
+                .mem_rsp_valid  (mem_rsp_valid),
+                .mem_rsp_data   (mem_rsp_data),
+                .mem_rsp_tag    (mem_rsp_tag),
+                .mem_rsp_ready  (mem_rsp_ready),
+
+                .busy           (busy));
   
-  VX_to_mem_bypass RAM(clk, reset, mem_req_valid, mem_req_ready, mem_req_rw, mem_req_addr, mem_req_tag, mem_req_data, mem_rsp_valid, mem_rsp_data, mem_rsp_tag, mem_rsp_ready);
+  VX_to_mem_bypass RAM(clk, 
+                       reset, 
+                       mem_req_valid, 
+                       mem_req_ready, 
+                       mem_req_rw, 
+                       mem_req_addr, 
+                       mem_req_tag, 
+                       mem_req_data, 
+                       mem_rsp_valid, 
+                       mem_rsp_data, 
+                       mem_rsp_tag, 
+                       mem_rsp_ready);
 );
 
 
