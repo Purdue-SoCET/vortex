01c7aeb77030032b2aa50406680bb90f766605b5
prakasr <prakasr@github.com>
2023-03-08 15:24:00 -0500

    improved app

--- tests/socet/main.c
+++ tests/socet/main.c
@@ -3,15 +3,19 @@
 #include "./include/vx_intrinsics.h"
 #include "./include/vx_spawn.h"
 
-
-//kernel function 
+//kernel function (reduction)
 int kernel_function(context_t cxt, void * arg){
 	int tid = vx_thread_id();
-	int a = 3;
-	int b = 4;
-	a = b + a + 3;
-	a += ((int *)arg)[tid];
-	return 0;
+	int sum = 0;
+	int div_test = 0;
+	//no locks in vortex 
+	sum += ((int *)arg)[tid];
+	__if (sum > 0)
+		div_test = 0;
+	__else
+		div_test = 1;
+	__endif
+	return sum;
 }
 
 int main(){
@@ -21,10 +25,15 @@ int main(){
 	kernel_context.global_offset[0] = 4;//gid
 	kernel_context.local_size[0] = 4;//lid
 	kernel_context.printf_buffer = NULL;
+	kernel_context.printf_buffer_position = NULL;
+	kernel_context.printf_buffer_capacity = 0;
 	kernel_context.work_dim = 1;
 	
 	//kernel arg
 	int global_buffer[64];
+	for (int i = 0; i < 64; i++){
+		global_buffer[i] = i;
+	}
 	
 	//spawn the kernel (pass the context, the kernel function and one arg)
 	vx_spawn_kernel(&kernel_context, (vx_spawn_kernel_cb) kernel_function, global_buffer);
