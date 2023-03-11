5fb0bd025847bcbe6e98ff77642a98deb8049a2a
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-03-10 19:44:31 -0500

    Update and rename page_table.v to page_table.sv

--- hw/rtl/page_table.sv
+++ hw/rtl/page_table.sv
@@ -1,20 +1,26 @@
-//Author: Raghul Prakash
+//Author: Raghul Prakash (socet33, prakasr)
 //page table cache in Vortex
 //takes vaddr in gives out paddr
 `include "ahb_if.vh"
+`include "VX_define.vh"
 
 module page_table_table(
-    input logic [31:0] vaddr, 
-    output logic [31:0] paddr, 
     input logic CLK,
     input logic nRST,
-    input logic error,
-    input logic finished,
+    output logic page_fault_reg,
     ahb_if.ahb_s ahb_s,
-    output logic [31:0][3:0] page_table_entry_reg
+
+    output wire                             mem_req_pt_valid,
+    input wire 				    mem_req_valid,
+    
+    output wire [`VX_MEM_ADDR_WIDTH-1:0]    mem_req_pt_addr,
+    input wire [`VX_MEM_ADDR_WIDTH-1:0]     mem_req_addr
     );
     
-
+    
+    	wire [`VX_MEM_ADDR_WIDTH-1:0] vaddr;
+    	assign vaddr = mem_req_addr;
+    
 	localparam WORD_W = 32;
 
 	//htrans
@@ -26,11 +32,15 @@ module page_table_table(
 	logic write_req, prev_write_req;
 	logic [WORD_W-1:0] wreq_addr; //write request address
 
-
-	//4 virtual pages, 8 physical pages
+	localparam NVPN = 8;
+	localparam log2NVP = 4 - 1;
+	//8 virtual pages, 4 physical pages
 	//ENTRY: PPN[3 bits] + VALID[1 bit] 
-	logic [31:0][3:0] page_table_entry; 
-	logic [31:0][3:0] n_page_table_entry;
+	logic [31:0][log2NVP:0] page_table_entry; 
+	logic [31:0][log2NVP:0] n_page_table_entry;
+
+	logic page_fault;
+	logic n_page_fault;
 
 	//logic for read
 	logic [WORD_W-1:0] read_data;
@@ -43,30 +53,33 @@ module page_table_table(
 	logic read_req;
 
 
+	//page table registers
 	always_ff @ (posedge CLK, negedge nRST) begin
 		if (nRST) begin
-			page_table_entry[0] <= 0;
-			page_table_entry[1] <= 0;
-			page_table_entry[2] <= 0;
-			page_table_entry[3] <= 0;`
-			page_table_entry[4] <= 0;
-			page_table_entry[5] <= 0;
-			page_table_entry[6] <= 0;
-			page_table_entry[7] <= 0;		
+			page_table_entry[0] <= 32'b0;
+			page_table_entry[1] <= 32'b0;
+			page_table_entry[2] <= 32'b0;
+			page_table_entry[3] <= 32'b0;
+			page_table_entry[4] <= 32'b0;
+			page_table_entry[5] <= 32'b0;
+			page_table_entry[6] <= 32'b0;
+			page_table_entry[7] <= 32'b0;
+			page_fault <= 0;		
 		end
 		else begin
-			page_table_entry[0] <= npage_table_entry[0];
-                       page_table_entry[1] <= npage_table_entry[1];
-                       page_table_entry[2] <= npage_table_entry[2];
-                       page_table_entry[3] <= npage_table_entry[3];
-                       page_table_entry[4] <= npage_table_entry[4];
-                       page_table_entry[5] <= npage_table_entry[5];
-                       page_table_entry[6] <= npage_table_entry[6];
-			page_table_entry[7] <= npage_table_entry[7];
+		       page_table_entry[0] <= n_page_table_entry[0];
+                       page_table_entry[1] <= n_page_table_entry[1];
+                       page_table_entry[2] <= n_page_table_entry[2];
+                       page_table_entry[3] <= n_page_table_entry[3];
+                       page_table_entry[4] <= n_page_table_entry[4];
+                       page_table_entry[5] <= n_page_table_entry[5];
+                       page_table_entry[6] <= n_page_table_entry[6];
+		       page_table_entry[7] <= n_page_table_entry[7];
+		       page_fault <= n_page_fault;
 		end
 	end	
 
-
+	//control signals
 	always_ff @ (posedge CLK, negedge nRST) begin
 	    if (nRST == 1'b0) begin
 		read_data <= 'h0;
@@ -119,15 +132,6 @@ module page_table_table(
 
 	end
 	
-	//assign to output
-	assign page_table_entry_reg[0] = page_table_entry[0];
-	assign page_table_entry_reg[1] = page_table_entry[1];
-	assign page_table_entry_reg[2] = page_table_entry[2];
-	assign page_table_entry_reg[3] = page_table_entry[3];
-	assign page_table_entry_reg[4] = page_table_entry[4];
-	assign page_table_entry_reg[5] = page_table_entry[5];
-	assign page_table_entry_reg[6] = page_table_entry[6];
-	assign page_table_entry_reg[7] = page_table_entry[7];
 
 	assign ahb_s.HRDATA = read_data; //can add "read_req? read_data: 'h0;" if needed 
 	//assign ahb_s.HREADY = ready; //Was HREADYOUT previously idk why
@@ -137,7 +141,28 @@ module page_table_table(
 
 	//page table entry translation
 	//calculate page number from virtual page
-	assign paddr = page_table_entry_reg[vaddr >> 4];
+	//16 KB pages (vaddr == 32 bits, VPN == vaddr 
+	//32 bits 0x0 addr vpn = 0
+	//16kb = 2^4 x 2^10 = 14 bits
+	logic valid;
+	logic [31:0] VPN;
+	logic [31:0] PPN;
+	assign VPN = (vaddr >> 13); //get page number
+	assign PPN = page_table_entry_reg[vaddr >> 13][log2NVP:0]; //get page number
+	assign valid = page_table_entry_reg[vaddr >> 13][4]; //valid bit 
 	
+	always_comb begin
+		paddr = 0;
+		n_page_fault = 0;
+		if (valid) begin
+			paddr = ((PPN << 13) | vaddr[12:0]); //check if valid (calculate paddr)
+		end
+		else begin
+			paddr = 32'hdeadbeef;
+			n_page_fault = 1; //page fault
+		end
+	end
+	
+	assign mem_req_pt_valid = mem_req_valid & ~page_fault;	
 
 end module
