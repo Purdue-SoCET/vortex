ae71bc145c18b8b75f286697c9e09c01cb22d424
Ansh Patel <pate1590@purdue.edu>
2023-03-22 17:05:04 -0400

    adding adapter with interfaces

--- hw/rtl/libs/VX_ahb_adapter.sv
+++ hw/rtl/libs/VX_ahb_adapter.sv
@@ -13,32 +13,25 @@ module VX_ahb_adapter #(
     input  wire                         clk,
     input  wire                         reset,
 
-    // Vortex request
-    input wire                          mem_req_valid,
-    input wire                          mem_req_rw,
-    input wire [VX_BYTEEN_WIDTH-1:0]    mem_req_byteen,
-    input wire [VX_ADDR_WIDTH-1:0]      mem_req_addr,
-    input wire [VX_DATA_WIDTH-1:0]      mem_req_data,
-    input wire [VX_TAG_WIDTH-1:0]       mem_req_tag, //IGNORE, useful for AXI
+    // // Vortex request
+    // input wire                          req.mem_req_valid,
+    // input wire                          req.mem_req_rw,
+    // input wire [VX_BYTEEN_WIDTH-1:0]    mem_req_byteen,
+    // input wire [VX_ADDR_WIDTH-1:0]      req.mem_req_addr,
+    // input wire [VX_DATA_WIDTH-1:0]      req.mem_req_data,
+    // input wire [VX_TAG_WIDTH-1:0]       req.mem_req_tag, //IGNORE, useful for AXI
 
-    // Vortex response
-    input wire                          mem_rsp_ready,
-    output wire                         mem_rsp_valid,        
-    output wire [VX_DATA_WIDTH-1:0]     mem_rsp_data,
-    output wire [VX_TAG_WIDTH-1:0]      mem_rsp_tag, //IGNORE, useful for AXI
-    output wire                         mem_req_ready,
+    // // Vortex response
+    // input wire                          rsp.mem_rsp_ready,
+    // output wire                         rsp.mem_rsp_valid,        
+    // output wire [VX_DATA_WIDTH-1:0]     rsp.mem_rsp_data,
+    // output wire [VX_TAG_WIDTH-1:0]      rsp.mem_rsp_tag, //IGNORE, useful for AXI
+    // output wire                         req.mem_req_ready,
 
 
-    // AHB CHANNELS
-    output wire HSEL,
-    output wire [AHB_ADDR_WIDTH-1:0] HADDR,
-    output wire [1:0] HTRANS,
-    output wire [2:0] HSIZE,
-    output wire HWRITE,
-    output wire [AHB_DATA_WIDTH-1:0] HWDATA,
-    input wire [AHB_DATA_WIDTH-1:0] HRDATA,
-    input wire HREADY,
-    input wire HRESP,
+    ahb_if.manager ahb,
+    VX_mem_rsp_if.master rsp,
+    VX_mem_req_if.slave req,
     input logic [3:0] count
     //to do: add burst
 );
@@ -94,20 +87,20 @@ module VX_ahb_adapter #(
         end
     end
 
-    assign full_addr = {mem_req_addr, 6'd0};
+    assign full_addr = {req.addr, 6'd0};
 
     always_comb begin : STATES_CTRL
         nxt_state = state;
         
-        casez(state)
+        case(state)
 
-            IDLE: nxt_state = (mem_req_valid) ? DATA : IDLE;
+            IDLE: nxt_state = (req.valid) ? DATA : IDLE;
 
             DATA: begin
-                if ((HREADY == 1) && (count == 4'd15)) begin
+                if ((ahb.HREADY == 1) && (count == 4'd15)) begin
                     nxt_state = COMPLETE;
                 end
-                else if (HRESP == 1)
+                else if (ahb.HRESP == 1)
                     nxt_state = ERROR;
                 else begin
                     nxt_state = DATA;
@@ -119,7 +112,7 @@ module VX_ahb_adapter #(
             end
 
             COMPLETE: begin
-                if (mem_rsp_ready == 1)
+                if (rsp.ready == 1)
                     nxt_state = IDLE;
                 else
                     nxt_state = COMPLETE;
@@ -128,149 +121,149 @@ module VX_ahb_adapter #(
     end
 
     always_comb begin : OUTPUTS
-        HSEL = 0;
-        HSIZE = '0;
-        HTRANS = '0;
-        HWRITE = 0;
-        HADDR = '0;
-        HWDATA = '0;
+        ahb.HSEL = 0;
+        ahb.HSIZE = '0;
+        ahb.HTRANS = '0;
+        ahb.HWRITE = 0;
+        ahb.HADDR = '0;
+        ahb.HWDATA = '0;
         nxt_addr = addr;
         nxt_rw = rw;
         nxt_data = data;
         nxt_data_read = data_read;
         count_en = 0;
         clear = 0;
-        mem_rsp_valid = 0;
-        mem_req_ready = 0;
+        rsp.valid = 0;
+        req.ready = 0;
 
         case(state)
             IDLE: begin
-                mem_req_ready = 1;
-                if (mem_req_valid == 1) begin
-                    HSEL = 1;
-                    HSIZE = 3'b010;
-                    HTRANS = 2'b10;
-                    HWRITE = mem_req_rw;
-                    HADDR = full_addr;
-                    nxt_data = mem_req_data;
+                req.ready = 1;
+                if (req.valid == 1) begin
+                    ahb.HSEL = 1;
+                    ahb.HSIZE = 3'b010;
+                    ahb.HTRANS = 2'b10;
+                    ahb.HWRITE = req.rw;
+                    ahb.HADDR = full_addr;
+                    nxt_data = req.data;
                     nxt_addr = full_addr;
-                    nxt_rw = mem_req_rw;
+                    nxt_rw = req.rw;
                     clear = 1;
                 end
             end
 
             DATA: begin 
-                if ((HREADY == 1)) begin
+                if ((ahb.HREADY == 1)) begin
                     count_en = 1;
-                    HSEL = 1;
-                    HSIZE = 3'b010;
-                    HTRANS = 2'b10;
-                    HWRITE = rw;
-                    HADDR = addr + 4;
+                    ahb.HSEL = 1;
+                    ahb.HSIZE = 3'b010;
+                    ahb.HTRANS = 2'b10;
+                    ahb.HWRITE = rw;
+                    ahb.HADDR = addr + 4;
                     nxt_addr = addr + 4;
                     case (count)
                         4'd0: begin
-                            nxt_data_read.data1 = HRDATA;
-                            HWDATA = data[31:0];
+                            nxt_data_read.data1 = ahb.HRDATA;
+                            ahb.HWDATA = data[31:0];
                         end
                         4'd1: begin
-                            nxt_data_read.data2 = HRDATA;
-                            HWDATA = data[63:32];
+                            nxt_data_read.data2 = ahb.HRDATA;
+                            ahb.HWDATA = data[63:32];
                         end
                         4'd2: begin
-                            nxt_data_read.data3 = HRDATA;
-                            HWDATA = data[95:64];
+                            nxt_data_read.data3 = ahb.HRDATA;
+                            ahb.HWDATA = data[95:64];
                         end
                         4'd3: begin
-                            nxt_data_read.data4 = HRDATA;
-                            HWDATA = data[127:96];
+                            nxt_data_read.data4 = ahb.HRDATA;
+                            ahb.HWDATA = data[127:96];
                         end
                         4'd4: begin
-                            nxt_data_read.data5 = HRDATA;
-                            HWDATA = data[159:128];
+                            nxt_data_read.data5 = ahb.HRDATA;
+                            ahb.HWDATA = data[159:128];
                         end
                         4'd5: begin
-                            nxt_data_read.data6 = HRDATA;
-                            HWDATA = data[191:160];
+                            nxt_data_read.data6 = ahb.HRDATA;
+                            ahb.HWDATA = data[191:160];
                         end
                         4'd6: begin
-                            nxt_data_read.data7 = HRDATA;
-                            HWDATA = data[223:196];
+                            nxt_data_read.data7 = ahb.HRDATA;
+                            ahb.HWDATA = data[223:196];
                         end
                         4'd7: begin
-                            nxt_data_read.data8 = HRDATA;
-                            HWDATA = data[255:224];
+                            nxt_data_read.data8 = ahb.HRDATA;
+                            ahb.HWDATA = data[255:224];
                         end
                         4'd8: begin
-                            nxt_data_read.data9 = HRDATA;
-                            HWDATA = data[287:256];
+                            nxt_data_read.data9 = ahb.HRDATA;
+                            ahb.HWDATA = data[287:256];
                         end
                         4'd9: begin
-                            nxt_data_read.data10 = HRDATA;
-                            HWDATA = data[319:288];
+                            nxt_data_read.data10 = ahb.HRDATA;
+                            ahb.HWDATA = data[319:288];
                         end
                         4'd10: begin
-                            nxt_data_read.data11 = HRDATA;
-                            HWDATA = data[351:320];
+                            nxt_data_read.data11 = ahb.HRDATA;
+                            ahb.HWDATA = data[351:320];
                         end
                         4'd11: begin
-                            nxt_data_read.data12 = HRDATA;
-                            HWDATA = data[383:352];
+                            nxt_data_read.data12 = ahb.HRDATA;
+                            ahb.HWDATA = data[383:352];
                         end
                         4'd12: begin
-                            nxt_data_read.data13 = HRDATA;
-                            HWDATA = data[415:384];
+                            nxt_data_read.data13 = ahb.HRDATA;
+                            ahb.HWDATA = data[415:384];
                         end
                         4'd13: begin
-                            nxt_data_read.data14 = HRDATA;
-                            HWDATA = data[447:416];
+                            nxt_data_read.data14 = ahb.HRDATA;
+                            ahb.HWDATA = data[447:416];
                         end
                         4'd14: begin
-                            nxt_data_read.data15 = HRDATA;
-                            HWDATA = data[479:448];
+                            nxt_data_read.data15 = ahb.HRDATA;
+                            ahb.HWDATA = data[479:448];
                         end
                         4'd15: begin
-                            nxt_data_read.data16 = HRDATA;
-                            HWDATA = data[511:480];
+                            nxt_data_read.data16 = ahb.HRDATA;
+                            ahb.HWDATA = data[511:480];
                         end
                     endcase
                 end
                 else
                 begin
                     count_en = 0;
-                    HSEL = 1;
-                    HSIZE = 3'b010;
-                    HTRANS = 2'b10;
-                    HWRITE = rw;
-                    HADDR = addr + 4;
+                    ahb.HSEL = 1;
+                    ahb.HSIZE = 3'b010;
+                    ahb.HTRANS = 2'b10;
+                    ahb.HWRITE = rw;
+                    ahb.HADDR = addr + 4;
                     case (count)
-                        4'd0: HWDATA = data[31:0];
-                        4'd1: HWDATA = data[63:32];
-                        4'd2: HWDATA = data[95:64];
-                        4'd3: HWDATA = data[127:96];
-                        4'd4: HWDATA = data[159:128];
-                        4'd5: HWDATA = data[191:160];
-                        4'd6: HWDATA = data[223:196];
-                        4'd7: HWDATA = data[255:224];
-                        4'd8: HWDATA = data[287:256];
-                        4'd9: HWDATA = data[319:288];
-                        4'd10: HWDATA = data[351:320];
-                        4'd11: HWDATA = data[383:352];
-                        4'd12: HWDATA = data[415:384];
-                        4'd13: HWDATA = data[447:416];
-                        4'd14: HWDATA = data[479:448];
-                        4'd15: HWDATA = data[511:480];
+                        4'd0: ahb.HWDATA = data[31:0];
+                        4'd1: ahb.HWDATA = data[63:32];
+                        4'd2: ahb.HWDATA = data[95:64];
+                        4'd3: ahb.HWDATA = data[127:96];
+                        4'd4: ahb.HWDATA = data[159:128];
+                        4'd5: ahb.HWDATA = data[191:160];
+                        4'd6: ahb.HWDATA = data[223:196];
+                        4'd7: ahb.HWDATA = data[255:224];
+                        4'd8: ahb.HWDATA = data[287:256];
+                        4'd9: ahb.HWDATA = data[319:288];
+                        4'd10: ahb.HWDATA = data[351:320];
+                        4'd11: ahb.HWDATA = data[383:352];
+                        4'd12: ahb.HWDATA = data[415:384];
+                        4'd13: ahb.HWDATA = data[447:416];
+                        4'd14: ahb.HWDATA = data[479:448];
+                        4'd15: ahb.HWDATA = data[511:480];
                     endcase
                 end
             end
 
             COMPLETE: begin
-                mem_rsp_valid = 1;
+                rsp.valid = 1;
             end  
         endcase
     end
 
-    assign mem_rsp_data = logic'(data_read);
+    assign rsp.data = (data_read);
 
     counter
     SIXTEEN(
