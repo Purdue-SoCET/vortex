121c449c94845323e8f106661a349734b940edf4
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-03-29 12:01:47 -0400

    setup tu

--- hw/tb/tex_unit/main.c
+++ hw/tb/tex_unit/main.c
@@ -1,1 +1,15 @@
 //Author: Raghul Prakash
+//Description: TU setup
+#include "./include/vx_intrinsics.h"
+#include "./include/vx_spawn.h"
+
+int main(){
+  //set up tu
+  csr_write(CSR_TEX_UNIT, 1);
+  csr_write(CSR_TEX_ADDR, image_file); //image address
+	csr_write(CSR_TEX_WIDTH, 64); //image address
+  csr_write(CSR_TEX_HEIGHT, 64); //image address
+  csr_write(CSR_TEX_FORMAT, TEX_FORMAT_R5G6B5); //format of image
+  
+	return 0;
+}
