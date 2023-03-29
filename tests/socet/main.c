b38d655e8e9de220c60cae4e90497e86ee2ec71f
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-03-29 19:23:36 -0400

    Update main.c

--- tests/socet/main.c
+++ tests/socet/main.c
@@ -3,46 +3,44 @@
 #include "./include/vx_intrinsics.h"
 #include "./include/vx_spawn.h"
 
-//kernel function (reduction)
-int kernel_function(context_t cxt, void * arg){
+//argument 
+typedef struct _argument_t {
+	int * vector_1;
+	int * vector_2;
+	int * vector_3;
+} argument_t;
+
+//kernel function (vector math)
+void kernel_function(context_t cxt, void * arg){
 	int tid = vx_thread_gid(); //thread id of current thread
-	int sum = 0;
-	int div_test = 0;
-	//no locks in vortex
-	int * currval = ((int *)arg)[tid];
-	sum += *currval;
+	arg->vector_3[tid] = arg->vector_1[tid] + arg->vector_2[id];
 	*currval += 1; //increments each value in the global array by 1
-	__if (sum > 0)
-		div_test = 0;
+	__if (arg->vector_3[tid] > 255)
+		arg->vector_3[tid] = 255;
 	__else
-		div_test = 1;
+		//
 	__endif
-	return sum;
+	return;
 }
+//global memory
+int vector_a[8] = {0,1,3,5,4,5,6,7};
+int vector_b[8] = {3,2,8,2,9,5,1,3};
+int vector_c[8];
 
 int main(){
 	//set up the context here
 	context_t kernel_context;
 	kernel_context.num_groups[0] = 1; //number of work groups (each workgroup maps to a thread block)
 	kernel_context.global_offset[0] = 0; //used to calculate the offset + global ID of a work-item
-	kernel_context.local_size[0] = 16;//lid local id
-	kernel_context.printf_buffer = NULL;
-	kernel_context.printf_buffer_position = NULL;
-	kernel_context.printf_buffer_capacity = 0;
+	kernel_context.local_size[0] = 8;//lid local id
 	kernel_context.work_dim = 1;
-        //gid = get_global_id( 0 ); get_global_id returned the index into the work for a given work item, each work item has different index
-        //numItems = get_local_size( 0 ); //number of local workitems per block
-        //tnum = get_local_id( 0 ); // thread number
-        //wgNum = get_group_id( 0 ); //work group number
-	//kernel arg
-	int global_buffer[16];
-	for (int i = 0; i < 16; i++){
-		global_buffer[i] = i;
-	}
-	
+	//setup the arguments
+	argument_t argument;
+	argument.vector_1 = vector_a;
+	argument.vector_2 = vector_b;
+	argument.vector_3 = vector_c;
 	//spawn the kernel (pass the context, the kernel function and one arg)
-	vx_spawn_kernel(&kernel_context, (vx_spawn_kernel_cb) kernel_function, global_buffer);
-
-	//vx_tmc(-1);
+	vx_spawn_kernel(&kernel_context, (vx_spawn_kernel_cb) kernel_function, &argument);
+	//vector_c will be finished with results now
 	return 0;
 }
