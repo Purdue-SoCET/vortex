d747f758fa65f3547ac8a19d387f8a024cc0c222
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-03-29 12:02:07 -0400

    Update main.c

--- hw/tb/tex_unit/main.c
+++ hw/tb/tex_unit/main.c
@@ -7,8 +7,8 @@ int main(){
   //set up tu
   csr_write(CSR_TEX_UNIT, 1);
   csr_write(CSR_TEX_ADDR, image_file); //image address
-	csr_write(CSR_TEX_WIDTH, 64); //image address
-  csr_write(CSR_TEX_HEIGHT, 64); //image address
+  csr_write(CSR_TEX_WIDTH, 64); //image size
+  csr_write(CSR_TEX_HEIGHT, 64); //image size
   csr_write(CSR_TEX_FORMAT, TEX_FORMAT_R5G6B5); //format of image
   
 	return 0;
