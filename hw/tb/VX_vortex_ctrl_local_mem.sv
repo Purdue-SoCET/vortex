be34ee8529df5930a4c92f85456f82624c0f209f
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-04-05 18:28:34 -0400

    Update VX_vortex_ctrl_local_mem.sv

--- hw/tb/VX_vortex_ctrl_local_mem.sv
+++ hw/tb/VX_vortex_ctrl_local_mem.sv
@@ -116,7 +116,7 @@ module VX_vortex_ctrl_local_mem #()(
 
         .busy           (busy),
 
-	.tb_addr_out_of_bounds (tb_addr_out_of_bounds).
+	    .tb_addr_out_of_bounds (tb_addr_out_of_bounds),
 	.gbif(gbif)
 	);
 
