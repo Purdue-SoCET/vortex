76fd7efe40e0a1b3ff62bfbe36b6d0163b324839
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-04-06 08:07:55 -0400

    Update VX_vortex_ctrl_local_mem.sv

--- hw/tb/VX_vortex_ctrl_local_mem.sv
+++ hw/tb/VX_vortex_ctrl_local_mem.sv
@@ -46,37 +46,39 @@ module VX_vortex_ctrl_local_mem #()(
 )
     // Memory Request:
     // vortex outputs
-    logic                             mem_req_valid,
-    logic                             mem_req_rw,
-    logic [`VX_MEM_BYTEEN_WIDTH-1:0]  mem_req_byteen, // 64 (512 / 8)
-    logic [`VX_MEM_ADDR_WIDTH-1:0]    mem_req_addr,   // 26
-    logic [`VX_MEM_DATA_WIDTH-1:0]    mem_req_data,   // 512
-    logic [`VX_MEM_TAG_WIDTH-1:0]     mem_req_tag,    // 56 (55 for SM disabled)
+    logic                             mem_req_valid,;
+    logic                             mem_req_rw;
+	logic [`VX_MEM_BYTEEN_WIDTH-1:0]  mem_req_byteen; // 64 (512 / 8)
+	logic [`VX_MEM_ADDR_WIDTH-1:0]    mem_req_addr;   // 26
+	logic [`VX_MEM_DATA_WIDTH-1:0]    mem_req_data;   // 512
+	logic [`VX_MEM_TAG_WIDTH-1:0]     mem_req_tag;    // 56 (55 for SM disabled)
     // vortex inputs
-    logic                            mem_req_ready,
+    logic                            mem_req_ready;
 
     // Memory response:
     // vortex inputs
-    logic                            mem_rsp_valid,        
-    logic [`VX_MEM_DATA_WIDTH-1:0]   mem_rsp_data,   // 512
-    logic [`VX_MEM_TAG_WIDTH-1:0]    mem_rsp_tag,    // 56 (55 for SM disabled)
+    logic                            mem_rsp_valid;        
+	logic [`VX_MEM_DATA_WIDTH-1:0]   mem_rsp_data;   // 512
+	logic [`VX_MEM_TAG_WIDTH-1:0]    mem_rsp_tag;    // 56 (55 for SM disabled)
     // vortex outputs
-    logic                             mem_rsp_ready,
+    logic                             mem_rsp_ready;
 
     // Status:
     // vortex outputs
-    logic                             busy,
+    logic                             busy;
 
     // tb:
-    logic                            tb_addr_out_of_bounds
+    logic                            tb_addr_out_of_bounds;
 
+    logic start;	
+	
     //vortex control slave
     Vortex_ctrl_slave vortex_ctrl_slave(clk, reset, busy, start, gbif);
 
     //vortex top
     VX_vortex Vortex(
         .clk            (clk),
-        .reset          (reset),
+	    .reset          (reset | start),
 
         .mem_req_valid  (mem_req_valid),
         .mem_req_rw     (mem_req_rw),
