bed138eed549025177c1b436c92bbe6b9bcb72d2
Raghul Prakash <prakasr@ececomp4.ecn.purdue.edu>
2023-04-07 10:03:57 -0400

    tex test compiles

--- tests/socet/main_tex.c
+++ tests/socet/main_tex.c
@@ -1,4 +1,4 @@
-nclude "./include/vx_intrinsics.h"
+#include "./include/vx_intrinsics.h"
 #include "./include/vx_spawn.h"
 
  int8_t image_file[] = {
@@ -74,7 +74,7 @@ int main(){
       csr_write(CSR_TEX_ADDR, image_file); //image address
         csr_write(CSR_TEX_WIDTH, 64); //image size
           csr_write(CSR_TEX_HEIGHT, 64); //image size
-           csr_write(CSR_TEX_FORMAT, TEX_FORMAT_R5G6B5); //format of image
+           csr_write(CSR_TEX_FORMAT, 1); //format of image
               //get data
                 uint32_t result = vx_tex(1, 0.1f, 0.1f, 1);
                   return 0;
