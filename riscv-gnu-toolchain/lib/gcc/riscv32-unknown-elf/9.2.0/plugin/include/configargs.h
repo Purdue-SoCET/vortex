245855fbcebdc6b74fbca648fd9b044c01fd22af
prakasr1208 <42628611+prakasr1208@users.noreply.github.com>
2023-04-21 10:50:04 -0400

    Delete riscv-gnu-toolchain directory

--- riscv-gnu-toolchain/lib/gcc/riscv32-unknown-elf/9.2.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/home/blaise/dev/riscv-gnu-toolchain/build/../riscv-gcc/configure --target=riscv32-unknown-elf --prefix=/home/blaise/dev/riscv-gnu-toolchain/build/../drops --disable-shared --disable-threads --enable-languages=c,c++ --with-system-zlib --enable-tls --with-newlib --with-sysroot=/home/blaise/dev/riscv-gnu-toolchain/build/../drops/riscv32-unknown-elf --with-native-system-header-dir=/include --disable-libmudflap --disable-libssp --disable-libquadmath --disable-libgomp --disable-nls --disable-tm-clone-registry --src=../../riscv-gcc --disable-multilib --with-abi=ilp32f --with-arch=rv32imf --with-tune=rocket 'CFLAGS_FOR_TARGET=-Os   -mcmodel=medlow' 'CXXFLAGS_FOR_TARGET=-Os   -mcmodel=medlow'";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "ilp32f" }, { "arch", "rv32imf" }, { "tune", "rocket" } };
